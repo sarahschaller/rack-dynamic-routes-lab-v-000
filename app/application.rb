776eb06098a3546c0776ca24efc31222b66f0bb4
Sarah Schaller <sarcsch@aol.com>
2017-09-21 20:26:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.find {|i| item.name == item_name}
+      if item = @@items.find {|i| i.name == item_name}
         resp.write item.price
       else
         resp.status = 400
